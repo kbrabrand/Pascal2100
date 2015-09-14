8060c1338bc28f766af0e3ee90fb95bf92d08862
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-09-14 22:24:14 +0200

    Remove trailing whitespace

--- tests/ScannerTest.java
+++ tests/ScannerTest.java
@@ -18,43 +18,32 @@ public class ScannerTest {
 		"endless_string_literal",
 		"illegal_character"
 	};
-	
-	private String processFile(String name) {
-		Runtime r = Runtime.getRuntime();
-		String output = "";
-		try {
-			Process p = r.exec("java -jar pascal2100.jar -testscanner ./test_files/" + name + ".pas");
-			p.waitFor();
-			
-			BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line;
 
-			while ((line = b.readLine()) != null) {
-				System.out.println(line);
-				
-				output += line;
-			}
+	private void processFile(String name) {
+		Runtime r = Runtime.getRuntime();
+
+        try {
+			Process p = r.exec("java -jar pascal2100.jar -testscanner tests/fixtures/" + name + ".pas");
+			p.waitFor();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return output;
 	}
-	
+
 	private String getTestFileContents(String name) throws Exception {
 		@SuppressWarnings("resource")
-		String content = new Scanner(new File("./test_files/" + name)).useDelimiter("\\Z").next();
-		
+		String content = new Scanner(new File("./tests/fixtures/" + name)).useDelimiter("\\Z").next();
+
 		return content;
 	}
-	
+
 	private void testProcessResult(String name) throws Exception {
 		processFile(name);
-		
+
 		String actual = getTestFileContents(name + ".log");
 		String expected = getTestFileContents(name + ".expected");
-		
+
 		assertEquals(expected, actual);
 		System.out.println(name + ": OK");
 	}
